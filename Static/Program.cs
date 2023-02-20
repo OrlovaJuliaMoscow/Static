6ba47a5bb180bad9d325f1eee0cc9dadb3f1ff6f
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-20 20:35:32 +0300

    Добавьте файлы проекта.

---
+++ Static/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r=Convert.ToDouble(Console.ReadLine());
+            double square = Circle.GetSquare(r);
+            double length = Circle.GetLength(r);
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double g = Circle.GetGipot(x, y);
+            Console.WriteLine($"{ square} \n{length}");
+            Console.ReadKey();
+                         
+            if (g <=r)
+            {
+                Console.WriteLine("Точка лежит в круге");
+                
+            }
+            else
+            {
+                Console.WriteLine("Точка не лежит в круге");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Static/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2*r*Math.PI;
+        }
+       
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public double GetGipot(double x, double y)
+        { 
+            return Math.Sqrt(x*x + y*y);
+        }
+    }
+}
